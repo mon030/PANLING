36fefe55278be566f0a4c79f618ec50de1b1cc9f
MON <42599449+mon030@users.noreply.github.com>
2023-09-24 21:20:06 -0500

    Remove Sidebar

--- docs/src/.vuepress/sidebar/zh-TW.ts
+++ docs/src/.vuepress/sidebar/zh-TW.ts
@@ -3,19 +3,6 @@ import { sidebar } from "vuepress-theme-hope";
 export const enSidebar = sidebar({
   "/": [
     "",
-    {
-      icon: "discover",
-      text: "Demo",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "Docs",
-      icon: "note",
-      prefix: "guide/",
-      children: "structure",
-    },
-    "slides",
+
   ],
 });
