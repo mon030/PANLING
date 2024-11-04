d3d1f3650cc062c8baa7a4c5fab9aa3c9e61c43d
mon030 <42599449+mon030@users.noreply.github.com>
2024-11-04 16:11:43 -0600

    Disable PWA

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -152,48 +152,5 @@ export default hopeTheme({
       vuePlayground: true,
     },
 
-     pwa: {
-       favicon: "/favicon.ico",
-       cacheHTML: true,
-       cachePic: true,
-      appendBase: true,
-       apple: {
-        icon: "/assets/icon/apple-icon-152.png",
-         statusBarColor: "black",
-       },
-       msTile: {
-         image: "/assets/icon/ms-icon-144.png",
-         color: "#ffffff",
-       },
-       themeColor: "#ff6347",
-
-       manifest: {
-         icons: [
-           {
-             src: "/assets/icon/chrome-mask-512.png",
-             sizes: "512x512",
-             purpose: "maskable",
-             type: "image/png",
-           },
-          {
-            src: "/assets/icon/chrome-mask-192.png",
-            sizes: "192x192",
-            purpose: "maskable",
-            type: "image/png",
-          },
-          {
-            src: "/assets/icon/chrome-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/assets/icon/chrome-192.png",
-            sizes: "192x192",
-            type: "image/png",
-           },
-         ],
-         
-       },
-     },
   },
 });
